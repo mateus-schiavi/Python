943619e4f169ea17facfccf486492490c192c438
Mateus Schiavi <mtsschiavi@gmail.com>
2022-10-07 20:10:46 -0300

    yes

--- background.py
+++ background.py
@@ -1,111 +1,93 @@
-# Example 1
-
-# Import the functions from the Draw 2-D library
-# so that they can be used in this program.
+# Importing functions
 import random
 import math
 from draw2d import \
     start_drawing, draw_line, draw_oval, draw_arc, \
     draw_rectangle, draw_polygon, draw_text, finish_drawing
 
-
 def main():
+    # Measurement for window
     scene_width = 800
     scene_height = 500
 
-    # Call the start_drawing function in the draw2d.py
-    # library which will open a window and create a canvas.
+    # Creating a window
     canvas = start_drawing("Scene", scene_width, scene_height)
 
-    # Call the draw_sky and draw_ground functions in this file.
+    # Calling functions
     draw_sky(canvas, scene_width, scene_height)
-    draw_ground(canvas, scene_width, scene_height)
-    draw_cloud(canvas, 525, 400)
-    draw_cloud(canvas, 450, 350)
-    draw_cloud(canvas, 525, 400)
-    draw_cloud(canvas, 450, 350)
-    draw_cloud(canvas, 525, 400)
-    draw_cloud(canvas, 450, 350)
+    draw_sun(canvas, 125, 75)
+    draw_cloud(canvas, 600, 350)
+    draw_cloud(canvas, 575, 325)
+    draw_cloud(canvas, 550, 325)
+    draw_cloud(canvas, 375, 400)
+    draw_cloud(canvas, 350, 415)
+    draw_cloud(canvas, 325, 375)
     draw_ground(canvas, scene_width, scene_height)
     for i in range(2000):
         x=random.randint(0, scene_width)
         y=random.randint(0, math.ceil(scene_height / 3))
         draw_grass(canvas, x, y)
+    draw_pine_tree(canvas, 100, 150, 140)
     draw_pine_tree(canvas, 125, 150, 125)
-    draw_pine_tree(canvas, 125, 150, 125)
-    draw_pine_tree(canvas, 125, 150, 125)
-    draw_pine_tree(canvas, 125, 150, 125)
-    
+    draw_pine_tree(canvas, 150, 150, 150)
+    draw_pine_tree(canvas, 130, 150, 130)
+    draw_pine_tree(canvas, 90, 150, 90)
+    draw_pine_tree(canvas, 115, 150, 115)
+    # draw_grid(canvas, scene_width, scene_height, 50) (COMMENTING OUT DRAW_GRID)
+
+
     # Call the finish_drawing function
-    # in the draw2d.py library.
     finish_drawing(canvas)
 
 
+# Defining functions  
 def draw_sky(canvas, scene_width, scene_height):
-    """Draw the sky and all the objects in the sky."""
     draw_rectangle(canvas, 0, scene_height / 3,
-        scene_width, scene_height, width=0, fill="sky blue")
+        scene_width, scene_height, width=0, fill="deepskyblue")
 
+def draw_sun(canvas, x, y):
+    draw_oval(canvas, x, y, x+150, y+150, outline="yellow", fill="yellow")
 
 def draw_ground(canvas, scene_width, scene_height):
-    """Draw the ground and all the objects on the ground."""
     draw_rectangle(canvas, 0, 0,
-        scene_width, scene_height / 3, width=0, fill="tan4")
-
-    # Draw a pine tree.
-    tree_center_x = 170
-    tree_bottom = 100
-    tree_height = 200
-    draw_pine_tree(canvas, tree_center_x,
-            tree_bottom, tree_height)
-
-    # Draw another pine tree.
-    tree_center_x = 90
-    tree_bottom = 70
-    tree_height = 220
-    draw_pine_tree(canvas, tree_center_x,
-            tree_bottom, tree_height)
-
-
-def draw_pine_tree(canvas, center_x, bottom, height):
-    """Draw a single pine tree.
-    Parameters
-        canvas: The canvas where this function
-            will draw a pine tree.
-        center_x, bottom: The x and y location in pixels where
-            this function will draw the bottom of a pine tree.
-        height: The height in pixels of the pine tree that
-            this function will draw.
-    Return: nothing
-    """
-    trunk_width = height / 10
-    trunk_height = height / 8
-    trunk_left = center_x - trunk_width / 2
-    trunk_right = center_x + trunk_width / 2
-    trunk_top = bottom + trunk_height
-
-    # Draw the trunk of the pine tree.
-    draw_rectangle(canvas,
-            trunk_left, trunk_top, trunk_right, bottom,
-            outline="gray20", width=1, fill="tan3")
-
-    skirt_width = height / 2
-    skirt_height = height - trunk_height
-    skirt_left = center_x - skirt_width / 2
-    skirt_right = center_x + skirt_width / 2
-    skirt_top = bottom + height
-
-    # Draw the crown (also called skirt) of the pine tree.
-    draw_polygon(canvas, center_x, skirt_top,
-            skirt_right, trunk_top,
-            skirt_left, trunk_top,
-            outline="gray20", width=1, fill="dark green")
-
-def draw_cloud(canvas, x, y):
-    draw_oval(canvas, x, y, x+125, y+70, outline="white", fill="white")
-
+        scene_width, scene_height / 3, width=0, fill="green")
+    
 def draw_grass(canvas, x, y):
     draw_rectangle(canvas,x,y,x+2,y+3, outline="green", fill="darkgreen")
-# Call the main function so that
-# this program will start executing.
+
+def draw_pine_tree(canvas, center_x, buttom, height):
+    # Draw the trunk of the tree
+    trunk_width = height / 5
+    trunk_height = height / 15
+    left_trunk = center_x - trunk_width / 15
+    buttom_trunk = buttom
+    right_trunk = center_x + trunk_width / 15
+    trunk_top = buttom + trunk_height
+    draw_rectangle(canvas, left_trunk, buttom_trunk, right_trunk, trunk_top, fill = 'tan4')
+    
+    # Draw the skirt of the tree
+    skirt_width = height / 2
+    skirt_left = center_x - skirt_width / 5
+    skirt_buttom = trunk_top
+    peak_x = center_x
+    peak_y = buttom + height
+    skirt_right = center_x + skirt_width / 5
+    draw_polygon(canvas, skirt_left, skirt_buttom, peak_x, peak_y, skirt_right, skirt_buttom, fill = 'forestGreen')
+
+def draw_cloud(canvas, x, y):
+    draw_oval(canvas, x, y, x+100, y+50, outline="white", fill="white")
+    
+def draw_grid(canvas, width, height, interval, color= 'blue'):
+    label_y = 15
+    for x in range(interval, width, interval):
+        draw_line(canvas, x, 0, x, height, fill=color)
+        draw_text(canvas, x, label_y, f"{x}", fill=color)
+
+    label_x = 15
+    for y in range(interval, height, interval):
+        draw_line(canvas, 0, y, width, y, fill=color)
+        draw_text(canvas, label_x, y, f"{y}", fill=color)
+
+
+# Calling the main function
 main()
