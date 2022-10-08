b1d56591e9d355191a5e7a26ef23942d2ab05f3c
Mateus Schiavi <mtsschiavi@gmail.com>
2022-10-08 00:33:14 -0300

    yes

--- hexagon.py
+++ hexagon.py
@@ -1,12 +1,12 @@
 import turtle
 
-colors = ["green","yellow","blue","white"]
+colors = ["Red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 p = turtle.Pen()
 turtle.bgcolor("black")
 
 for i in range(300):
-    p.pencolor(colors[i%4])
+    p.pencolor(colors[i%7])
     p.width(i/100+2)
     p.forward(i)
     p.left(59)
