3e0dbe94f1beddfdfd2220992678b1ae73614970
Mateus Schiavi <mtsschiavi@gmail.com>
2022-10-20 01:16:28 -0300

    yes

--- test_sentences.py
+++ test_sentences.py
@@ -1,44 +1,157 @@
+from sentences import get_determiner, get_noun, get_verb, get_preposition, get_prepositional_phrase
 import random
-
 import pytest
 
-from sentences import get_determiner, get_noun, get_verb
 
 
 def test_get_determiner():
-    # 1. Test the single determiners.
 
     single_determiners = ["a", "one", "the"]
 
-    # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
     for _ in range(4):
 
-        # Call the get_determiner function which
-        # should return a single determiner.
         word = get_determiner(1)
 
-        # Verify that the word returned from get_determiner
-        # is one of the words in the single_determiners list.
         assert word in single_determiners
 
-    # 2. Test the plural determiners.
+    plural_determiners = ["two", "some", "many", "the"]
 
-    plural_determiners = ["some", "many", "the"]
-
-    # This loop will repeat the statements inside it 4 times.
     for _ in range(4):
 
-        # Call the get_determiner function which
-        # should return a plural determiner.
-        word = get_determiner(2)
+        quantity = random.randint(2, 11)
 
-        # Verify that the word returned from get_determiner
-        # is one of the words in the plural_determiners list.
+        word = get_determiner(quantity)
+
         assert word in plural_determiners
         
-# Call the main function that is part of pytest so that the
-# computer will execute the test functions in this file.
+def test_get_noun():
+    
+    single_nouns = ['bird', "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"]
+    
+    for _ in range(10):
+    
+        noun = get_noun(1)
+    
+        assert noun in single_nouns
+        
+    plural_nouns = ["birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"]
+    
+    for _ in range(10):
+        
+        quantity = random.randint(2, 11)
+        
+        noun = get_noun(quantity)
+        
+        assert noun in plural_nouns
+        
+def test_get_verb():
+    past_verbs = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
+    
+    for _ in range(4):
+        
+        past_verb = get_verb(1, "past")
+        
+        assert past_verb in past_verbs
+        
+    single_present = ["drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"]
+    
+    for _ in range (4):
+        
+        single_present_verb = get_verb(1, "present")
+        
+        assert single_present_verb in single_present
+    
+    plural_present = ["drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"]
+    
+    for _ in range (4):
+        
+        quantity = random.randint(2, 11)
+        
+        plural_present_verb = get_verb(quantity, "present")
+        
+        assert plural_present_verb in plural_present
+        
+    future_tense = ["will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
+    
+    
+    for _ in range(4):
+        
+        quantity = random.randint(2, 11)
+        
+        future = get_verb(quantity, "future")
+        
+        assert future in future_tense
+
+def test_get_preposition():
+    
+    prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+    
+    preposition = get_preposition()
+    
+    assert preposition in prepositions
+
+def test_prepositional_phrase():
+    for _ in range(30):
+        
+        single_determiners = ["a", "one", "the"]
+        
+        single_nouns = ['bird', "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"]
+        
+        prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+        
+        single_prepositional_phrase = get_prepositional_phrase(1)
+        
+        list = single_prepositional_phrase.split()
+        
+        list_length = len(list)
+        
+        assert list_length == 3
+        assert list[0] in prepositions
+        assert list[1] in single_determiners
+        assert list[2] in single_nouns
+    
+def test_prepositional_phrase():
+    for _ in range(30):
+        
+        plural_determiners = ["two", "some", "many", "the"]
+        
+        plural_nouns = ["birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"]
+        
+        prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+        
+        plural_prepositional_phrase = get_prepositional_phrase(0)
+        
+        list = plural_prepositional_phrase.split()
+        
+        list_length = len(list)
+        
+        assert list_length == 3
+        assert list[0] in prepositions
+        assert list[1] in plural_determiners
+        assert list[2] in plural_nouns
+        
 pytest.main(["-v", "--tb=line", "-rN", __file__])
