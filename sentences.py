de8cb42cfb1b88c1bebb21903732ecc3afc932ac
Mateus Schiavi <mtsschiavi@gmail.com>
2022-10-20 23:04:06 -0300

    yes

--- sentences.py
+++ sentences.py
@@ -3,35 +3,22 @@ import random
 def main():
     tenses = ['past', 'present', 'future']
     quantities = [1, 2]
-    for quantity in quantities:
+    for x in quantities:
         for tense in tenses:
-            adj = get_adjective()
-            adv = get_adverb()
-            d = get_determiner(quantity)
-            n = get_noun(quantity)
-            v = get_verb(quantity, tense)
-            p1 = get_prepositional_phrase(quantity)
-            p2 = get_prepositional_phrase(quantity)
+            adjective = get_adjective()
+            adverb = get_adverb()
+            determiner = get_determiner(x)
+            noun = get_noun(quantity)
+            verb = get_verb(quantity, tense)
+            preposition_1 = get_prepositional_phrase(x)
+            preposition_2 = get_prepositional_phrase(x)
             
-            sentence = f'{p1.capitalize()}, {d} {adj} {n} {v} {adv} {p2}. \n'
+            sentence = f'{preposition_1.capitalize()}, {determiner} {adjective} {noun} {verb} {adverb} {preposition_2}. \n'
             
             print(sentence)
 
 
-def get_determiner(quantity):
-    '''Return a randomly chosen determiner. A determiner is
-    a word like 'the', 'a', 'one', 'some', 'many'.
-    If quantity == 1, this function will return either 'a',
-    'one', or 'the'. Otherwise this function will return
-    either 'some', 'many', or 'the'.
-    Parameter
-        quantity: an integer.
-            If quantity == 1, this function will return a
-            determiner for a single noun. Otherwise this
-            function will return a determiner for a plural
-            noun.
-    Return: a randomly chosen determiner.
-    '''
+def get_determiner(x):
     if quantity == 1:
         words = ['a', 'one', 'the']
     else:
@@ -43,29 +30,15 @@ def get_determiner(quantity):
 
 
 def get_noun(quantity):
-    '''Return a randomly chosen noun.
-    If quantity == 1, this function will
-    return one of these ten single nouns:
-        'bird', 'boy', 'car', 'cat', 'child',
-        'dog', 'girl', 'man', 'rabbit', 'woman'
-    Otherwise, this function will return one of
-    these ten plural nouns:
-        'birds', 'boys', 'cars', 'cats', 'children',
-        'dogs', 'girls', 'men', 'rabbits', 'women'
-    Parameter
-        quantity: an integer that determines if
-            the returned noun is single or plural.
-    Return: a randomly chosen noun.
-    '''
-    
+
     if quantity == 1:
-        nouns = ['bird', 'boy', 'car', 
-                 'cat', 'child', 'dog', 
+        nouns = ['butterfly', 'motorcycle', 'board', 
+                 'cat', 'child', 'snake', 
                  'girl', 'man', 'rabbit', 
                  'woman']
     else:
-        nouns = ['birds', 'boys', 'cars', 
-                 'cats', 'children', 'dogs', 
+        nouns = ['butterflies', 'motorcycles', 'boards', 
+                 'cats', 'children', 'snakes', 
                  'girls', 'men', 'rabbits', 
                  'women']
 
@@ -74,51 +47,27 @@ def get_noun(quantity):
 
 
 def get_verb(quantity, tense):
-    '''Return a randomly chosen verb. If tense is 'past',
-    this function will return one of these ten verbs:
-        'drank', 'ate', 'grew', 'laughed', 'thought',
-        'ran', 'slept', 'talked', 'walked', 'wrote'
-    If tense is 'present' and quantity is 1, this
-    function will return one of these ten verbs:
-        'drinks', 'eats', 'grows', 'laughs', 'thinks',
-        'runs', 'sleeps', 'talks', 'walks', 'writes'
-    If tense is 'present' and quantity is NOT 1,
-    this function will return one of these ten verbs:
-        'drink', 'eat', 'grow', 'laugh', 'think',
-        'run', 'sleep', 'talk', 'walk', 'write'
-    If tense is 'future', this function will return one of
-    these ten verbs:
-        'will drink', 'will eat', 'will grow', 'will laugh',
-        'will think', 'will run', 'will sleep', 'will talk',
-        'will walk', 'will write'
-    Parameters
-        quantity: an integer that determines if the
-            returned verb is single or plural.
-        tense: a string that determines the verb conjugation,
-            either 'past', 'present' or 'future'.
-    Return: a randomly chosen verb.
-    '''
-    
+
     if tense == 'past':
-        verbs = ['drank', 'ate', 'grew', 
-                 'laughed', 'thought', 'ran', 
-                 'slept', 'talked', 'walked', 
+        verbs = ['slept', 'swam', 'grew', 
+                 'laughed', 'ran', 'played', 
+                 'opened', 'talked', 'walked', 
                  'wrote']
     elif tense == 'present':
         if quantity == 1:
-            verbs = ['drinks', 'eats', 'grows', 
-                     'laughs', 'thinks', 'runs', 
-                     'sleeps', 'talks', 'walks', 
+            verbs = ['sleeps', 'swims', 'grows', 
+                     'laughs', 'runs', 'plays', 
+                     'opens', 'talks', 'walks', 
                      'writes']
         else:
-            verbs = ['drink', 'eat', 'grow', 
-                     'laugh', 'think', 'run', 
-                     'sleep', 'talk', 'walk', 
+            verbs = ['sleep', 'swim', 'grow', 
+                     'laugh', 'run', 'play', 
+                     'open', 'talk', 'walk', 
                      'write']
     elif tense == 'future':
-        verbs = ['will drink', 'will eat', 'will grow', 
-                 'will laugh', 'will think', 'will run', 
-                 'will sleep', 'will talk', 'will walk', 
+        verbs = ['will sleep', 'will swim', 'will grow', 
+                 'will laugh', 'will run', 'will play', 
+                 'will open', 'will talk', 'will walk', 
                  'will write']
 
     verb = random.choice(verbs)
@@ -126,16 +75,7 @@ def get_verb(quantity, tense):
 
 
 def get_preposition():
-    '''Return a randomly chosen preposition
-    from this list of prepositions:
-        'about', 'above', 'across', 'after', 'along',
-        'around', 'at', 'before', 'behind', 'below',
-        'beyond', 'by', 'despite', 'except', 'for',
-        'from', 'in', 'into', 'near', 'of',
-        'off', 'on', 'onto', 'out', 'over',
-        'past', 'to', 'under', 'with', 'without'
-    Return: a randomly chosen preposition.
-    '''
+
     prepositions = ['about', 'above', 'across', 
                     'after', 'along', 'around', 
                     'at', 'before', 'behind', 
@@ -151,16 +91,7 @@ def get_preposition():
 
 
 def get_prepositional_phrase(quantity):
-    """Build and return a prepositional phrase composed of three
-    words: a preposition, a determiner, and a noun by calling the
-    get_preposition, get_determiner, and get_noun functions.
-    Parameter
-        quantity: an integer that determines if the determiner
-            and noun in the prepositional phrase returned from
-            this function are single or plural.
-    Return: a prepositional phrase.
-    """
-    
+
     preposition = get_preposition()
     determiner = get_determiner(quantity)
     noun = get_noun(quantity)
@@ -171,28 +102,11 @@ def get_prepositional_phrase(quantity):
 
 
 def get_adjective():
-    adjectives = ['adorable', 'adventurous', 'aggressive', 
-                   'agreeable', 'alert', 'alive', 
-                   'amused', 'angry', 'annoyed', 
-                   'annoying', 'anxious', 'arrogant', 
-                   'ashamed', 'attractive', 'average', 
-                   'awful', 'bad', 'beautiful', 
+    adjectives = ['awful', 'bad', 'beautiful', 
                    'better', 'bewildered', 'black', 
                    'bloody', 'blue', 'blue-eyed', 
                    'blushing', 'bored', 'brainy', 
-                   'brave', 'breakable', 'bright', 
-                   'busy', 'calm', 'careful', 
-                   'cautious', 'charming', 'cheerful', 
-                   'clean', 'clear', 'clever', 
-                   'cloudy', 'clumsy', 'colorful', 
-                   'combative', 'comfortable', 'concerned', 
-                   'condemned', 'confused', 'cooperative', 
-                   'courageous', 'crazy', 'creepy', 
-                   'crowded', 'cruel', 'curious', 
-                   'cute', 'dangerous', 'dark', 
-                   'dead', 'defeated', 'defiant', 
-                   'delightful', 'depressed', 'determined', 
-                   'different', 'difficult', 'disgusted', 
+                   'brave', 'breakable', 'bright',
                    'distinct', 'disturbed', 'dizzy', 
                    'doubtful', 'drab', 'dull']
     
@@ -201,39 +115,13 @@ def get_adjective():
     
     
 def get_adverb():
-    adverbs = ['accidentally', 'always', 'angrily', 
-               'anxiously', 'arrogantly', 'awkwardly', 
-               'badly', 'blindly', 'boastfully', 
-               'boldly', 'bravely', 'brightly', 
-               'cheerfully', 'coyly', 'crazily', 
-               'cruelly', 'defiantly', 'deftly', 
-               'deliberately', 'devotedly', 'doubtfully', 
-               'dramatically', 'dreamily', 'dutifully', 
-               'eagerly', 'elegantly', 'enormously', 
-               'evenly', 'eventually', 'exactly', 
-               'faithfully', 'finally', 'fondly', 
-               'foolishly', 'fortunately', 'frantically', 
-               'frequently', 'gleefully', 'gracefully', 
-               'happily', 'hastily', 'honestly', 
-               'hopelessly', 'hourly', 'hungrily', 
-               'innocently', 'inquisitively', 'irritably', 
-               'jealously', 'joyously', 'justly', 
-               'kindly', 'lazily', 'loosely', 
+    adverbs = ['kindly', 'lazily', 'loosely', 
                'madly', 'merrily', 'mortally', 
                'mysteriously', 'nervously', 'never', 
                'noisily', 'obediently', 'obnoxiously', 
                'occasionally', 'often', 'only', 
                'perfectly', 'politely', 'poorly', 
                'powerfully', 'promptly', 'quickly', 
-               'rapidly', 'rarely', 'regularly', 
-               'reluctantly', 'rudely', 'safely', 
-               'seldom', 'selfishly', 'seriously', 
-               'shakily', 'sharply', 'silently', 
-               'slowly', 'solemnly', 'sometimes', 
-               'speedily', 'sternly', 'technically', 
-               'tediously', 'unexpectedly', 'usually', 
-               'victoriously', 'vivaciously', 'warmly', 
-               'wearily', 'weekly', 'wildly', 
                'yearly']
     
     adverb = random.choice(adverbs)
--- test_sentences.py
+++ test_sentences.py
@@ -3,195 +3,133 @@ import pytest
 
 
 def test_get_determiner():
-    """Call and test the get_determiner function 
-    Parameters:
-        none 
-    """
-    # 1. Test the single determiners.
 
     single_determiners = ["a", "one", "the"]
 
-    # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
     for _ in range(4):
 
-        # Call the get_determiner function which
-        # should return a single determiner.
         word = get_determiner(1).lower()
 
-        # Verify that the word returned from get_determiner
-        # is one of the words in the single_determiners list.
         assert word in single_determiners
 
-    # 2. Test the plural determiners.
 
     plural_determiners = ["some", "many", "the"]
 
-    # This loop will repeat the statements inside it 4 times.
     for _ in range(4):
 
-        # Call the get_determiner function which
-        # should return a plural determiner.
         word = get_determiner(2).lower()
 
-        # Verify that the word returned from get_determiner
-        # is one of the words in the plural_determiners list.
         assert word in plural_determiners
 
 
 def test_get_noun():
-    """Call and test the get_noun function 
-    Parameters:
-        none 
-    """
-    # 1. Test the single nouns.
 
-    single_nouns = ['bird', 'boy', 'car', 'cat', 'child', 'dog', 'girl', 'man', 'rabbit', 'woman']
+    single_nouns = ['butterfly', 'motorcycle', 'board', 
+                 'cat', 'child', 'snake', 
+                 'girl', 'man', 'rabbit', 
+                 'woman']
 
-    # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
     for _ in range(4):
 
-        # Call the get_determiner function which
-        # should return a single determiner.
         word = get_noun(1)
 
-        # Verify that the word returned from get_noun
-        # is one of the words in the single_nouns list.
         assert word in single_nouns
 
-    # 2. Test the plural nouns.
 
-    plural_nouns = ['birds', 'boys', 'cars', 'cats', 'children', 'dogs', 'girls', 'men', 'rabbits', 'women']
+    plural_nouns = ['butterflies', 'motorcycles', 'boards', 
+                 'cats', 'children', 'snakes', 
+                 'girls', 'men', 'rabbits', 
+                 'women']
 
-    # This loop will repeat the statements inside it 4 times.
     for _ in range(4):
 
-        # Call the get_noun function which
-        # should return a plural noun.
         word = get_noun(2)
 
-        # Verify that the word returned from get_determiner
-        # is one of the words in the plural_determiners list.
         assert word in plural_nouns
 
 
 def test_get_verb():
-    """Call and test the get_verb function 
-    Parameters:
-        none 
-    """
-    # 1. Test the past verbs.
     
-    past_verbs = ['drank', 'ate', 'grew', 'laughed', 'thought', 'ran', 'slept', 'talked', 'walked', 'wrote']
+    past_verbs = ['slept', 'swam', 'grew', 
+                 'laughed', 'ran', 'played', 
+                 'opened', 'talked', 'walked', 
+                 'wrote']
     
     for _ in range(4):
-        # Call the get_verb function which
-        # should return a past tense verb.
         word = get_verb(1, 'past')
         
-        # Verify that the word returned from get_verb
-        # is one of the words in the past_verbs list.
         assert word in past_verbs
         
         
-    # 2. Test the present singular verbs.  
     
-    singular_present_verbs = ['drinks', 'eats', 'grows', 'laughs', 'thinks', 'runs', 'sleeps', 'talks', 'walks', 'writes']
+    singular_present_verbs = ['sleeps', 'swims', 'grows', 
+                     'laughs', 'runs', 'plays', 
+                     'opens', 'talks', 'walks', 
+                     'writes']
     
     for _ in range(4):
-        # Call the get_verb function which
-        # should return a singular present tense verb.
         word = get_verb(1, 'present')
         
-        # Verify that the word returned from get_verb
-        # is one of the words in the singular_present_verbs list.
         assert word in singular_present_verbs
         
         
-    # 3. Test the present plural verbs.
     
-    plural_present_verbs = ['drink', 'eat', 'grow', 'laugh', 'think', 'run', 'sleep', 'talk', 'walk', 'write']
+    plural_present_verbs = ['sleep', 'swim', 'grow', 
+                     'laugh', 'run', 'play', 
+                     'open', 'talk', 'walk', 
+                     'write']
     
     for _ in range(4):
-        # Call the get_verb function which
-        # should return a singular present tense verb.
         word = get_verb(2, 'present')
         
-        # Verify that the word returned from get_verb
-        # is one of the words in the singular_present_verbs list.
         assert word in plural_present_verbs
               
         
-    # 4. Test the future verbs.
     
-    future_verbs = ['will drink', 'will eat', 'will grow', 'will laugh', 'will think', 'will run', 'will sleep', 'will talk', 'will walk', 'will write']
+    future_verbs = ['will sleep', 'will swim', 'will grow', 
+                 'will laugh', 'will run', 'will play', 
+                 'will open', 'will talk', 'will walk', 
+                 'will write']
     
     for _ in range(4):
-        # Call the get_verb function which
-        # should return a future tense verb.
         word = get_verb(2, 'future')
         
-        # Verify that the word returned from get_verb
-        # is one of the words in the singular_present_verbs list.
         assert word in future_verbs
     
     
 def test_get_preposition():
-    """Call and test the get_preposition function
-    Parameters:
-        none
-    """
     
-    prepositions = ['about', 'above', 'across', 'after', 'along',
-        'around', 'at', 'before', 'behind', 'below',
-        'beyond', 'by', 'despite', 'except', 'for',
-        'from', 'in', 'into', 'near', 'of',
-        'off', 'on', 'onto', 'out', 'over',
-        'past', 'to', 'under', 'with', 'without']
+    prepositions = ['about', 'above', 'across', 
+                    'after', 'along', 'around', 
+                    'at', 'before', 'behind', 
+                    'below', 'beyond', 'by', 
+                    'despite', 'except', 'for', 
+                    'from', 'in', 'into', 
+                    'near', 'of', 'off', 
+                    'on', 'onto', 'out', 
+                    'over', 'past', 'to', 
+                    'under', 'with', 'without']
     
     for _ in range(4):
         
-        # Call the get_preposition function which
-        # should return a preposition.
         word = get_preposition()
         
-        # Verify that the word returned from get_preposition
-        # is one of the words in the prepositions list.
         assert word in prepositions
     
     
 def test_get_prepositional_phrase():
-    """Call and test the get_prepositional_phrase function 
-    Parameters:
-        none 
-    """
     
     for _ in range(4):
         
-        # Call the get_prepositional_phrase function which
-        # should return a phrase.
         phrase =  get_prepositional_phrase(1).split(' ')
 
-        # Verify that the word count returned from get_prepositional_phrase
-        # is equal to 4.
         assert len(phrase) == 4
         
     for _ in range(4):
         
-        # Call the get_prepositional_phrase function which
-        # should return a phrase.
         phrase =  get_prepositional_phrase(2).split(' ')
         
-        # Verify that the word count returned from get_prepositional_phrase
-        # is equal to 4.
         assert len(phrase) == 4
+
     
-    
-# Call the main function that is part of pytest so that the
-# computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
