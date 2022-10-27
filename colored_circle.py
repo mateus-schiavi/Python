87aaf81ca20dd1d67cb4aadef7362518ab8b0d83
Mateus Schiavi <mtsschiavi@gmail.com>
2022-10-27 18:27:50 -0300

    yes

--- colored_circle.py
+++ colored_circle.py
@@ -6,14 +6,16 @@ ninja.speed(10)
 
 
 for i in range(180):
-    ninja.color("red")
+    turtle.bgcolor("black")
+    ninja.color("green")
     ninja.forward(100)
     ninja.right(30)
-    ninja.color("blue")
+    ninja.color("yellow")
     ninja.forward(20)
     ninja.left(60)
-    ninja.color("green")
+    ninja.color("blue")
     ninja.forward(50)
+    ninja.color("white")
     ninja.right(30)
 
     ninja.penup()
