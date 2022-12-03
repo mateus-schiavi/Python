80d05e4a594364881e9bee8f2e9f627a5a4e14ca
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 21:49:35 -0300

    yes

--- complex.py
+++ complex.py
@@ -6,7 +6,7 @@ x = int(input("Enter the real number of the equation: "))
 y = int(input("Enter the imaginary number of the equation: "))
 print(complex(x + 1j*y))
  
-plt.plot(x, y) 
+plt.plot(x, y, '-.') 
 plt.xlabel("Real") 
 plt.ylabel("Imaginary") 
 plt.title("Solving Complex Equations") 
