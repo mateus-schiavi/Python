0e8940603aa4474df44455928efcf3ed38197ee2
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 23:42:56 -0300

    yes

---
+++ testing_complex.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import cmath
+import pytest
+
+def function_one():
+    data = np.arange(12) + 1j*np.arange(-4, 8) 
+    x = data.real 
+    y = data.imag 
+    plt.plot(x, y, '-.r*') 
+    plt.ylabel('Imaginary') 
+    plt.xlabel('Real') 
+    plt.show()
+
+def function_two():
+    data = np.array([1+5j, 2-1j, -1j, -8, 4+3j, 3+2j, -2+6j, 5]) 
+    x = data.real 
+    y = data.imag 
+    plt.plot(x, y, 'g*') 
+    plt.ylabel('Imaginary') 
+    plt.xlabel('Real') 
+    plt.show()
+    
+def function_three():
+    data = np.arange(10) + 1j*np.arange(0,10)
+    x = data.real
+    y = data.imag
+    plt.plot(x,y, 'g*')
+    plt.ylabel('Imaginary')
+    plt.xlabel('Real')
+    plt.show()
+pytest.main(["-v", "--tb=line", "-rN", __file__]) 
