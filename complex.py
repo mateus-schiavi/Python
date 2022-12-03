28265c70cf749a0dc58564fa7cc4115424bd063c
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 21:45:36 -0300

    yes

--- complex.py
+++ complex.py
@@ -5,17 +5,9 @@ import cmath
 x = int(input("Enter the real number of the equation: "))
 y = int(input("Enter the imaginary number of the equation: "))
 print(complex(x + 1j*y))
-
-x1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) 
-y1 = x1 + 1j
-  
+ 
 plt.plot(x, y) 
 plt.xlabel("Real") 
 plt.ylabel("Imaginary") 
 plt.title("Solving Complex Equations") 
 plt.show()  
-plt.figure() 
-x1 = [2, 4, 6, 8] 
-y1 = [3, 5, 7, 9] 
-plt.plot(x1, y1, '-.') 
-plt.show() 
